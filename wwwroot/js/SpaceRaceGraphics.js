557bce723506a5bde09da358fe2d6224c8de9e19
Brandon Walker <lyshine@live.com>
2020-04-20 23:13:08 -0600

    Minor physics changes to ship gravity.

--- wwwroot/js/SpaceRaceGraphics.js
+++ wwwroot/js/SpaceRaceGraphics.js
@@ -5,25 +5,24 @@
 let canvas, ctx;
 let ships = [];
 let bodies = [];
+let gravitation = 500;
 
 window.addEventListener("load", () => {
     canvas = document.getElementById("game");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.addEventListener("mousedown", (e) => {
-        animate();
-    });
+    canvas.width = 800;
+    canvas.height = 800;
 
     ctx = canvas.getContext("2d");
-    bodies.push(new body(20, 2, [canvas.width / 2, canvas.clientHeight / 2]));
-    ships.push(new Ship(20, 30, [1200, 800], [0, 0], [0, 0]));
-    animate();
+    bodies.push(new body(20, 50, canvas.width / 2, canvas.clientHeight / 2));
+    bodies.push(new body(10, 50, 500, 700));
+    ships.push(new Ship(20, 30, 750, 750, 0, -8));
+    setInterval(() => { animate() }, 10);
 });
 
 
 function animate() {
     // clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // draw celectial bodies
     bodies.forEach(body => body.draw(ctx));
@@ -32,14 +31,13 @@ function animate() {
 }
 
 class Ship {
-    constructor(size, color, position, velocity, acceleration) {
+    constructor(size, color, xPosition, yPosition, xVelocity, yVelocity) {
         this.size = size;
         this.color = color;
-        this.position = position;
-        if (velocity.length != 2) this.velocity = [0, 0];
-        else this.velocity = velocity;
-        if (acceleration.length != 2) this.acceleration = [0, 0];
-        else this.acceleration = acceleration;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
     }
 
     draw(ctx) {
@@ -48,34 +46,38 @@ class Ship {
         // draw ship
         ctx.beginPath();
         ctx.fillStyle = "rgb(255, 0, 0)";
-        ctx.fillRect(this.position[0], this.position[1], this.size, this.size);
+        ctx.fillRect(this.xPosition, this.yPosition, this.size, this.size);
     }
 
     updatePosition() {
-        // calculate change in x and y
+        // calculate change in x and y for a single step in time
         bodies.forEach(body => {
-            let distanceX = body.position[0] - this.position[0];
-            let distanceY = body.position[1] - this.position[1];
-            this.velocity[0] = -5;
+            let a = body.xPosition - this.xPosition; // distance x from body
+            let b = body.yPosition - this.yPosition; // distance y from body
+            let distance = Math.sqrt((a * a) + (b * b)); // distance between ship and body
+            let force = (gravitation * body.mass) / (distance * distance); // attraction applied
+            let angle = Math.atan2(a, b);
+            this.xVelocity += Math.sin(angle) * force;
+            this.yVelocity += Math.cos(angle) * force;
         });
-        this.position[0] += this.velocity[0];
-        this.position[1] += this.velocity[1];
+        this.xPosition += this.xVelocity;
+        this.yPosition += this.yVelocity;
     }
 }
 
 class body {
-    constructor(size, mass, position) {
+    constructor(size, mass, xPosition, yPosition) {
         this.size = size;
         this.mass = mass;
-        if (position.length != 2) this.position = [0, 0];
-        else this.position = position;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
     }
 
     draw(ctx) {
         // draw celetial body
         ctx.beginPath();
         ctx.fillStyle = "rgb(0, 255, 0)";
-        ctx.arc(this.position[0], this.position[1], this.size, 0, 2 * Math.PI);
+        ctx.arc(this.xPosition, this.yPosition, this.size, 0, 2 * Math.PI);
         ctx.fill();
     }
 }
